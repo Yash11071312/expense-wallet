f60daed9946e4c815e4b614bc44d88aace66ee6e
Yash Khedekar <ykhedekar1107@gmail.com>
2026-07-02 19:48:36 +0530

    added the add income and expenses js and some styling

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let line1 = document.querySelector(".line1");
 let line2 = document.querySelector(".line2");
 let line3 = document.querySelector(".line3");
 let home = document.querySelector("#home");
+let AddIncomeBox = document.querySelector(".AddIncomeBox")
+let amountInput = document.querySelector("#AmountIp");
+let balance = document.querySelector("#balance");
+let transactions =document.querySelector("#transactions")
+let totalBalance = 0;
+let tiger =0;
+let transaction= "None"
+let transactionfirst= 0
 function hideAllPages() {
     landing.classList.add("hidden");
     main.classList.add("hidden");
@@ -19,18 +27,21 @@ AddClasslistHidden(line3);
 
 AddClasslistHidden(line2);
 RemoveClasslistHidden(line1)
-RemoveClasslistClicked(about)
-RemoveClasslistClicked(features)
+
+}
+function landingPage() {
+    hideAllPages();
+    landing.classList.remove("hidden");
 }
 function featuresPage() {
-
+hideAllPages();
+features.classList.remove("hidden")
 AddClasslist(features);
 AddClasslistHidden(line1);
 
 AddClasslistHidden(line3);
 RemoveClasslistHidden(line2)
-RemoveClasslistClicked(home)
-RemoveClasslistClicked(about)
+
   
 }
 function aboutPage() {
@@ -72,3 +83,79 @@ document.querySelector("#features").addEventListener("click", (e) => {
 document.querySelector("#about").addEventListener("click", (e) => {
   aboutPage();
 });
+function AddIncome() {
+
+  AddIncomeBox.classList.remove("none")
+tiger=1;
+}
+function Addbtn(e){
+     e.preventDefault();
+
+    let amount = Number(amountInput.value);
+
+  
+
+if (tiger==1){
+    totalBalance += amount;
+
+    balance.innerText = `₹${totalBalance}`;
+    transactionsftn(amount)
+    transaction="income"
+    amountInput.value = "";
+    
+    AddIncomeBox.classList.add("none");
+  }
+  else{
+    if (amount<=totalBalance) {
+    totalBalance -= amount;
+    transactionsftn(amount)
+    transaction="expense"
+  balance.innerText = `₹${totalBalance}`;
+  
+  amountInput.value = "";
+  
+  AddIncomeBox.classList.add("none");
+}
+else{
+  alert("expense is greater than ur wallet money")
+}
+}}
+
+function CancelBtn(){
+  AddIncomeBox.classList.add("none")
+  
+}
+function Expensededuct() {
+  AddIncomeBox.classList.remove("none")
+  tiger=0;
+}
+
+function transactionsftn(amt) {
+  if (transactionfirst==0) {
+    
+    if (transaction=="income") {
+      
+      
+      transactions.innerHTML=`<br>₹${amt} Credited`
+      transaction="income"
+    }
+    else{
+    
+      transactions.innerHTML=`<br>₹${amt} Debited`
+      transaction="expense"
+    }
+transactionfirst=1;
+  }
+  else{
+
+  
+    if (transaction=="income") {
+      
+      transactions.innerHTML+=`<br>₹${amt} Credited`
+    }
+    else{
+      transactions.innerHTML+=`<br>₹${amt} Debited`
+      
+    }
+  }
+  }
